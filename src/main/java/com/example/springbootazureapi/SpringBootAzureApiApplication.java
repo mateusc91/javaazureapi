c29f047eba06f5050ebde44cf6d9a65978e451e7
mateusc91 <mateusc.eng@gmail.com>
2023-01-04 15:00:42 -0300

    adding get endpoint

--- src/main/java/com/example/springbootazureapi/SpringBootAzureApiApplication.java
+++ src/main/java/com/example/springbootazureapi/SpringBootAzureApiApplication.java
@@ -6,8 +6,21 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
+@RestController
 public class SpringBootAzureApiApplication {
+
+    @GetMapping("/test")
+    public List<String> message() {
+        List<String> students = new ArrayList<>();
+        students.add("Victor");
+        students.add("Mateus");
+
+        return students;
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAzureApiApplication.class, args);
     }
